70884c418ebe8e5a6d79e4a9431ab5ed8ee59ea6
Guillaume <guillaumeHoude@users.noreply.github.com>
2022-03-16 21:14:26 -0400

    Better status code and errors

--- controllers/api2Controller.js
+++ controllers/api2Controller.js
@@ -5,9 +5,11 @@ const https = require('https')
 const jsonfile = require('jsonfile')
 const { Buffer } = require('buffer')
 const createError = require('http-errors')
+const path = require('path')
+const fs = require('fs')
 
 /// CONSTANTS ///
-const jsonFileInput = './files/api2input.json' //using simple jsonFile as DB as the whole data set fits in memory and we prevent a network error
+const jsonFileInput = './files/api2input.json' //jsonFile as DB as data fits in memory and prevent network error
 const jsonFileOutput = './files/api2output.json'
 const usefullVars = {modelName: 'MODEL_NAME_NM', makeName: 'MAKE_NAME_NM', year: 'DATE_YEAR_CD', categoryEn: 'CATEGORY_ETXT', categoryFr: 'CATEGORY_FTXT'}
 
@@ -28,14 +30,18 @@ exports.get_recalls = function (req, res) {
           return recall.category.CATEGORY_ETXT.toLowerCase() === req.query.category.toLowerCase() || recall.category.CATEGORY_FTXT.toLowerCase() === req.query.category.toLowerCase()          
         })
 
-        res.json(filtered)
+        res.status(200).json(filtered)
       }else{
-        res.json(obj)
+        res.status(200).json(obj)
       }
     })
     .catch(error => {
       console.error(error)
-      res.send(error)
+      if(error.code === 'ENOENT'){
+        res.status(204).send('Nothing to return yet')
+      }else{
+        res.status(500).send(error)
+      }
     })
 }
 
@@ -49,50 +55,65 @@ exports.get_single = function (req, res) {
         //Filter by category
         let filtered = obj.filter(recall => {return recall.recallNumber === req.query.recallNum})
 
-        res.json(filtered[0])
+        res.status(200).json(filtered[0])
       }else{
-        res.send(createError(400, '400 - request requires a recall number'))
+        res.status(400).send('400 - request requires a recall number')
       }
     })
     .catch(error => {
       console.error(error)
-      res.send(error)
+      res.status(500).send(error)
     })
 }
 
 //POST
 exports.post_new = function (req, res) {
   console.log('POST file')
-  let buf = Buffer.from(req.file.buffer, 'ascii')
-  let recalls = JSON.parse(buf.toString())
-  
-  jsonfile.writeFile(jsonFileInput, recalls)
-    .then(result => {
-      console.log(`file received`)
-      res.json({ 'file received': true})
+  if(req.file !== 'undefined' && req.file && req.file.buffer.length > 0){
+    let buf = Buffer.from(req.file.buffer, 'ascii')
+    let recalls = JSON.parse(buf.toString())
+    
+    if (!fs.existsSync(path.dirname(jsonFileInput))){
+      fs.mkdirSync(path.dirname(jsonFileInput));
+    }
+    if (!fs.existsSync(path.dirname(jsonFileOutput))){
+      fs.mkdirSync(path.dirname(jsonFileOutput));
+    }
+    
+    jsonfile.writeFile(jsonFileInput, recalls)
+      .then(result => {
+        console.log(`file received`)
+        res.status(201).send('input file saved')
+      })
+      .catch(error => {
+        console.error(error)
+        res.status(500).send(error)
+      })
+   
+    Promise.all(recalls.map(recall => {
+      return new Promise((resolve, reject) => {
+        findCategories(recall)
+          .then(newRecall =>{
+            resolve(newRecall)
+          })
+          .catch(error => {
+            console.error(error)
+            console.log('oldRecall')
+            reject(recall)
+          })
+      })
+    })) //Promise.all complete, then
+      .then(values => {
+        jsonfile.writeFile(jsonFileOutput, values)
+          .then(result => {
+            console.log(`output file saved`)
+          })
+          .catch(error => console.error(error))
     })
-    .catch(error => console.error(error))
- 
-  Promise.all(recalls.map(recall => {
-    return new Promise((resolve, reject) => {
-      findCategories(recall)
-        .then(newRecall =>{
-          resolve(newRecall)
-        })
-        .catch(error => {
-          console.error(error)
-          console.log('oldRecall')
-          reject(recall)
-        })
-    })
-  })) //Promise.all complete, then
-    .then(values => {
-      jsonfile.writeFile(jsonFileOutput, values)
-        .then(result => {
-          console.log(`file written`)
-        })
-        .catch(error => console.error(error))
-  })
+  }else{
+    console.log('here 406')
+    res.status(406).send('406 - NOT Acceptable')
+  }
 }
 
 /// UTILITIES ///
